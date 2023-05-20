6090a5c48274ad1e2b86a47e43e17020e01db3c8
Nelson M C Delva <79728556+nelson3design@users.noreply.github.com>
2023-05-20 08:55:14 -0300

    Update adiciona-action.php

--- adiciona-action.php
+++ adiciona-action.php
@@ -133,3 +133,121 @@ $conn->close();
 
 
 ?>
+
+<?php
+
+// Verificar se o formulário foi submetido
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Obter o ID do post e o ID do usuário
+
+    $post_id = $_POST['post_id'];
+
+    $user_id = $_POST['user_id'];
+
+    
+
+    // Obter o tipo de ação (like ou dislike)
+
+    $action = $_POST['action'];
+
+    // Conexão com o banco de dados (substitua as informações de acordo com o seu ambiente)
+
+    $servername = "localhost";
+
+    $username = "seu_usuario";
+
+    $password = "sua_senha";
+
+    $dbname = "seu_banco_de_dados";
+
+    // Conexão com o banco de dados
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar conexão
+
+    if ($conn->connect_error) {
+
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+
+    }
+
+    // Verificar se o usuário já registrou uma ação para esse post
+
+    $sql = "SELECT * FROM likes WHERE post_id = $post_id AND user_id = $user_id";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+
+        // O usuário já registrou uma ação para esse post, então atualizar a ação
+
+        $sql = "UPDATE likes SET action = '$action' WHERE post_id = $post_id AND user_id = $user_id";
+
+        if ($conn->query($sql) === TRUE) {
+
+            echo "Ação atualizada com sucesso.";
+
+        } else {
+
+            echo "Erro ao atualizar ação: " . $conn->error;
+
+        }
+
+    } else {
+
+        // Inserir uma nova ação para o post
+
+        $sql = "INSERT INTO likes (post_id, user_id, action) VALUES ($post_id, $user_id, '$action')";
+
+        if ($conn->query($sql) === TRUE) {
+
+            echo "Ação registrada com sucesso.";
+
+        } else {
+
+            echo "Erro ao registrar ação: " . $conn->error;
+
+        }
+
+    }
+
+    $conn->close();
+
+}
+
+?>
+
+<!-- Exibir o conteúdo e os botões de "like" e "dislike" -->
+
+<div>
+
+    <!-- Exibir o conteúdo do post -->
+
+    <p>Conteúdo do post...</p>
+
+    <!-- Formulário para o botão de "like" -->
+
+    <form method="POST" action="">
+
+        <input type="hidden" name="post_id" value="1"> <!-- Substitua pelo ID do post atual -->
+
+        <input type="hidden" name="user_id" value="1"> <!-- Substitua pelo ID do usuário atual -->
+
+        <input type="hidden" name="action" value="like">
+
+        <input type="submit" value="Like">
+
+    </form>
+
+    <!-- Formulário para o botão de "dislike" -->
+
+    <form method="POST" action="">
+
+        <input type="hidden" name="post_id" value="1"> <!-- Substitua pelo ID do post atual -->
+
+        <input type="hidden" name="user_id" value="1"> <!-- Substitua pelo ID do usuário
+
+
